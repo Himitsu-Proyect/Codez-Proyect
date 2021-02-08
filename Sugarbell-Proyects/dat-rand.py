5b50a3a164dca52010057b5216e38fb24b5a363b
Sugarbell <67601252+TaprisSugarbell@users.noreply.github.com>
2021-02-08 17:04:40 -0600

    random data 'str' with copy

--- Sugarbell-Proyects/dat-rand.py
+++ Sugarbell-Proyects/dat-rand.py
@@ -11,3 +11,6 @@ copy(rastr)
 print(rastr)
 
 
+'''
+Frase1,Frase2,Frase3,Frase4,Frase5,Frase6,Frase7,Frase8,Frase9,Frase10,Frase11,Frase12,Frase13,Frase14,Frase15,Frase16,Frase17,Frase18,Frase19,Frase20
+'''
